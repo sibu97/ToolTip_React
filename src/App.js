8e6b8bf29522349bc33abe9714c93e83f35def1b
SIBANANDA NAYAK <nayaksibananda22@gmail.com>
2022-06-29 23:49:21 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
+import "./styles.css";
 
-function App() {
+export  function App() {
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+       <h1>Tool_Tip</h1>
+       <div class="tooltip">
+      <Tippy content="Tooltip_TOP" interactive={true} interactiveBorder={20} delay={100} placement= 'top'>
+      <button id="t1" class="btn tippy" >hover here</button>
+        </Tippy></div>
+      
+    	<div class="tooltip">
+      <Tippy content="Tooltip_BOTTOM" interactive={true} interactiveBorder={20} delay={100} placement= 'bottom'>
+      <button id="t2" class="btn tippy" >hover here</button></Tippy></div>
+
+    	<div class="tooltip">
+      <Tippy content="Tooltip_RIGHT" interactive={true} interactiveBorder={20} delay={100} placement= 'right'>
+      <button id="t3" class="btn tippy" >hover here</button></Tippy></div>
+
+    	<div class="tooltip">
+      <Tippy content="Tooltip_LEFT" interactive={true} interactiveBorder={20} delay={100} placement= 'left'>
+      <button id="t4" class="btn tippy" >hover here</button></Tippy></div>
+      
+
+    </>
+   );
 }
 
 export default App;
